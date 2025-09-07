d2561ecb3064408e65e7d4e7ca65f8fcc3e280e3
sebasvalencia09-netizen <sebasvalencia09@gmail.com>
2025-09-06 22:39:03 -0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Bienvenido a Flash Repartidores 🚀</h1>
+      <p>Tu aplicación está corriendo con Next.js 14.</p>
+    </main>
+  );
+}
